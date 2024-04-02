e601b5ac996b21854fa991e8bd3c1c8f2d2b2c57
Prashant Katiyar <prashantkat10796@gmail.com>
2024-04-02 23:21:54 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,15 @@
+data = [
+    {
+        name: "prashant katiyar",
+        age: 30
+    },
+    {
+        name: "Rakesh",
+        age: 32
+    },
+    {
+        name: "shubham",
+        age: 33
+    }
+]
+module.exports = data;
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let a= 10;
-console.log(a);
+const http = require('http');
+const data = require('./data')
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'application\json'})
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(4000);
+
+
+
+let a = 10;
+let b= 30 ;
+
+let waitingDta =  new Promise ((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve(50)
+    },2000)
+})
+
+waitingDta.then((data)=>{
+   console.log(a+data);
+})
