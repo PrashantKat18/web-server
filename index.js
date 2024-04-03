78ef886dce46609f29c53dad734dbf473bb9c6cf
Prashant Katiyar <prashantkat10796@gmail.com>
2024-04-03 23:46:34 +0530

    install express js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-const http = require('http');
-const data = require('./data')
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'application\json'})
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(4000);
+const express = require('express');
 
+const app = express();
 
-
-let a = 10;
-let b= 30 ;
-
-let waitingDta =  new Promise ((resolve,reject)=>{
-    setTimeout(()=>{
-      resolve(50)
-    },2000)
+app.get('',(req,res)=>{
+  res.send("hello this is home page")
 })
 
-waitingDta.then((data)=>{
-   console.log(a+data);
+app.get('/about',(req,res)=>{
+  res.send("hello this is about page")
 })
+
+app.listen(4000);
+
